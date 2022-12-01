3797c8ca308bd60f12bb962c6e8c37cb7b4b31c5
Пользователь <Valenta2910@mail.ru>
2022-12-01 20:32:00 +0300

    задание 1 сделал инициализацию переменных разных типов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        byte cat = 1;
+        System.out.println( "Значение переменной cat с типом byte равно" + cat);
+        short dog = 2;
+        System.out.println( "Значение переменной dog с типом short равно" + dog);
+        int bird = 3;
+        System.out.println( "Значение переменной bird с типом int равно" + bird);
+        float bear = 4.5f;
+        System.out.println( "Значение переменной bear с типом float равно" + bear);
+        long wolf = 5L;
+        System.out.println( "Значение переменной wolf с типом long равно" + wolf);
+        double snake = 6.5;
+        System.out.println( "Значение переменной snake с типом double равно" + snake);
     }
 }
