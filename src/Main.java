8694ae0538a745d53c92a0a03c8b49011318d871
Пользователь <Valenta2910@mail.ru>
2022-12-03 17:25:21 +0300

    задание 4. Нахожу количество бутылок за 1 мин, за 20, за сутки, за трое суток и за месяц.

--- src/Main.java
+++ src/Main.java
@@ -31,5 +31,24 @@ public class Main {
         int student =  (LyudmilaPavlovna + AnnaSergeyevna + EkaterinaAndreevna);
         int LeafOnStudent = Paper / student;
         System.out.println("На каждого ученика рассчитано " + LeafOnStudent +  " листов бумаги.");
+
+        System.out.println("Задание 4");
+        int bottle = 16;
+        int PerMin = 16 / 2;
+        int Twentymin = PerMin * 20;
+        System.out.println("За 20 минут машина произвела " + Twentymin +  " штук бутылок");
+        int day = PerMin * 60 * 24;
+        System.out.println("За сутки машина произвела " + day +  " штук бутылок");
+        int ThreeDay = day * 3;
+        System.out.println("За трое суток машина произвела " + ThreeDay +  " штук бутылок");
+        int month = day * 30;
+        System.out.println("За месяц машина произвела " + month +  " штук бутылок");
+
+
+
+
+
+
+
     }
 }
