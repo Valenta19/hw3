4872fc4bc62061e8b3408bf34863111805df3d5d
Пользователь <Valenta2910@mail.ru>
2022-12-04 14:18:11 +0300

    проверил, исправил недочеты.

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
         System.out.println("Задание 2");
         float a = 27.12f;
-        long b = 987678965549L;
+        long b = 987_678_965_549L;
         double c = 2.786;
         short d = 569;
         int i = -159;
@@ -24,70 +24,73 @@ public class Main {
         byte g = 67;
 
         System.out.println( "Задание 3" );
-        int LyudmilaPavlovna = 23;
-        int AnnaSergeyevna = 27;
-        int EkaterinaAndreevna = 30;
+        int lyudmilaPavlovna = 23;
+        int annaSergeyevna = 27;
+        int ekaterinaAndreevna = 30;
         int Paper = 480;
-        int student =  (LyudmilaPavlovna + AnnaSergeyevna + EkaterinaAndreevna);
+        int student =  (lyudmilaPavlovna + annaSergeyevna + ekaterinaAndreevna);
         int LeafOnStudent = Paper / student;
         System.out.println("На каждого ученика рассчитано " + LeafOnStudent +  " листов бумаги.");
 
         System.out.println("Задание 4");
         int bottle = 16;
-        int PerMin = 16 / 2;
-        int Twentymin = PerMin * 20;
-        System.out.println("За 20 минут машина произвела " + Twentymin +  " штук бутылок");
-        int day = PerMin * 60 * 24;
+        int perMin = 16 / 2;
+        int twentyMin =perMin * 20;
+        System.out.println("За 20 минут машина произвела " + twentyMin +  " штук бутылок");
+        int day = perMin * 60 * 24;
         System.out.println("За сутки машина произвела " + day +  " штук бутылок");
-        int ThreeDay = day * 3;
-        System.out.println("За трое суток машина произвела " + ThreeDay +  " штук бутылок");
+        int threeDay = day * 3;
+        System.out.println("За трое суток машина произвела " + threeDay +  " штук бутылок");
         int month = day * 30;
         System.out.println("За месяц машина произвела " + month +  " штук бутылок");
 
         System.out.println("Задание 5");
         int paints = 120;
-        int WhitePaint = 2;
-        int BrownPaint = 4;
-        int ColorsPerClass = WhitePaint + BrownPaint;
-        int classes = paints / ColorsPerClass;
-        int AllWhite = classes * WhitePaint;
-        int AllBrown = classes * BrownPaint;
-        System.out.println("В школе, где " + classes + " классов, нужно " + AllWhite + " банок белой краски и " + AllBrown + " банок коричневой краски");
+        int whitePaint = 2;
+        int brownPaint = 4;
+        int colorsPerClass = whitePaint + brownPaint;
+        int classes = paints / colorsPerClass;
+        int allWhite = classes * whitePaint;
+        int allBrown = classes * brownPaint;
+        System.out.println("В школе, где " + classes + " классов, нужно " + allWhite + " банок белой краски и " + allBrown + " банок коричневой краски");
 
         System.out.println("Задание 6 ");
         int banana = 80;
         int milk = 105;
-        int Cream = 100;
+        int cream = 100;
         int agg = 70;
-        double TotalProducts = banana * 5 +  milk * 2 + Cream * 2 + agg * 4;
-        System.out.println(TotalProducts + " грамм ");
-        double KilogramF = TotalProducts / 1000;
+        double totalProducts = banana * 5 +  milk * 2 + cream * 2 + agg * 4;
+        System.out.println(totalProducts + " грамм ");
+        double KilogramF = totalProducts / 1_000;
         System.out.println(KilogramF + " килограмм");
 
         System.out.println(" Задание 7 ");
-        int excessWeight = 7 * 1000;
-        int reset250 = excessWeight / 250;
+        int excessWeight = 7_000;
+        int lossWeightPerDayMin = 250;
+        int lossWeightPerDayMax = 500;
+        double reset250 = (double) excessWeight / lossWeightPerDayMin;
+        double reset500 = (double) excessWeight / lossWeightPerDayMax;
+        double average = ( lossWeightPerDayMin + lossWeightPerDayMax) / 2D;
+        double averageDaysCount = excessWeight / average;
         System.out.println(reset250 + " дней ");
-        int reset500 = excessWeight / 500;
         System.out.println(reset500 + " дней ");
-        int average = (reset250 + reset500) / 2;
-        System.out.println(average + " в среднем дней");
+        System.out.println(averageDaysCount + " в среднем дней");
 
         System.out.println("Задание 8");
 
-        int salaryMasha = 67760;
+        int salaryMasha = 67_760;
         int increaseMasha = salaryMasha / 10;
         int newSalaryMasha = salaryMasha + increaseMasha;
         int differenceIncomeMasha = increaseMasha * 12;
         System.out.println("Маша теперь получает " + newSalaryMasha +  " рублей. Годовой доход вырос на " + differenceIncomeMasha + " рублей.");
 
-        int salaryDenis = 83690;
+        int salaryDenis = 83_690;
         int increaseDenis = salaryDenis / 10;
         int newSalaryDenis = salaryDenis + increaseDenis;
         int differenceIncomeDenis = increaseDenis * 12;
         System.out.println("Денис теперь получает " + newSalaryDenis +  " рублей. Годовой доход вырос на " + differenceIncomeDenis + " рублей.");
 
-        int salaryKristina = 76230;
+        int salaryKristina = 76_230;
         int increaseKristina = salaryKristina / 10;
         int newSalaryKristina = salaryKristina + increaseKristina;
         int differenceIncomeKristina = increaseKristina * 12;
